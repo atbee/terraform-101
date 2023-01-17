2e59ce378c39acdbc8a1ae76d32bbc6de6655dea
atbee <athibet.pi@gmail.com>
2023-01-17 15:39:31 +0700

    feat: specific provider version :bee:

--- file/main.tf
+++ file/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.2.3"
+    }
+  }
+}
+
 resource "local_file" "this" {
   content  = "Hello Terraform"
   filename = "my_file.txt"
