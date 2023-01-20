0a54100c20496cd4202588782b0e092e5ff8b6a8
atbee <athibet.pi@gmail.com>
2023-01-20 11:03:12 +0700

    feat: set up custom backend

--- aws_instance/provider.tf
+++ aws_instance/provider.tf
@@ -5,4 +5,10 @@ terraform {
       version = "4.51.0"
     }
   }
+
+  backend "s3" {
+    bucket = "terraform-101-backend"
+    key    = "states/aws_instance/"
+    region = "ap-southeast-1"
+  }
 }
